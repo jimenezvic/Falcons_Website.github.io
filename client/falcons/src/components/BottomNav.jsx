dbb90f4b15d618aa92454a0b86e9a547435656ea
Ivan Jimenez <jimenezvic@outlook.com>
2024-08-19 13:45:09 -0500

    links-update/navbar

--- client/falcons/src/components/BottomNav.jsx
+++ client/falcons/src/components/BottomNav.jsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './BottomNav.css'; 
 
-function BottomNav(){
+function BottomNav() {
   return (
-<>
-
-    <nav className="nav-second">
-      
-    </nav>
-
-</>
-
-  
+    <>
+      <nav className="nav-second flex">
+        <div className="logo">
+          <h1>FALCONS</h1>
+        </div>
+        <ul className="flex">
+          <li><Link to="/proyect" className="links">Proyect</Link></li>
+          <li><Link to="/registration" className="links">Registration</Link></li>
+          <li><Link to="/" className="links">Matches</Link></li>
+          <li><Link to="/policies" className="links">Register</Link></li>
+        </ul>
+      </nav>
+    </>
   );
 };
+
 export default BottomNav;
+
