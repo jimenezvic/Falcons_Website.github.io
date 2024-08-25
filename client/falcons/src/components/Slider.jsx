8eff2b523efbe4bb2bcf1e6364bfb23c6e1cb368
Ivan Jimenez <jimenezvic@outlook.com>
2024-08-24 21:58:20 -0500

    footeer

--- client/falcons/src/components/Slider.jsx
+++ client/falcons/src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Card from './Card'
+import Players from "./Players"
 import '../components/Slider.css';
 
 function Slider() {
@@ -32,30 +33,36 @@ function Slider() {
   const slides = [
 {
   image: '/images/home.jpg',
-  title: 'Slider 1',
-  description: "Lorem"
+  title: 'WELCOME TO FALCONS ACADEMY',
+  description: "Lorem",
+  footer: 'Join a legacy of excellence. At Falcons Academy, every step you take brings you closer to greatness. Embrace the journey and soar to new heights with us.'
+
 },
 {
   image: '/images/dude2.jpg',
-  title: 'Slider 2',
-  description: "Lorem"
+  title: 'GET YOUR FALCON ID',
+  description: "Lorem",
+  footer: 'Unlock your potential. Your Falcon ID is more than just a number—it is your key to a community that believes in your success and future.'
 },
 {
   image: '/images/players.jpg',
-  title: 'Slider 3',
-  description: "Lorem"
+  title: 'YOUR SUCCESS STARTS HERE',
+  description: "Lorem",
+  footer: 'Every dream needs a starting point. At Falcons Academy, we provide the foundation for your success. Take the first step and make it count.'
+
 },
 {
   image: '/images/main.jpg',
-  title: 'Slider 4',
-  description: "Lorem"
+  title: 'FIND YOUR PLACE HERE',
+  description: "Lorem",
+  footer: 'Belong, grow, and thrive. Falcons Academy is where you find your purpose, passion, and people. Your journey starts with us'
+
 }
   ]
 
   return (
     <>
     <div className="body">
-
       <div className="slider">
         <div className="list">
           {slides.map((slide, index)=>(
@@ -66,6 +73,9 @@ function Slider() {
               <h2>{slide.title}</h2>
               <p>{slide.description}</p>
               </div>
+              <div className='ex'>
+                {slide.footer}
+              </div>
               </div>
           ))}
         </div>
@@ -74,29 +84,13 @@ function Slider() {
             <button id='next' onClick={goToNextSlide}><i class="fa-solid fa-greater-than"></i></button>
           </div> */}
       </div>
+      
 
-      <div className="card-cont">
-        <div className="card">
-            <Card />
-        </div>
-        <div className="card">
-            <Card />
-        </div>
-        
-      </div>
-      <div className="card-cont">
-        <div className="card">
-            <Card />
-        </div>
-        <div className="card">
-            <Card />
-        </div>
-        
-      </div>
+      
 
-    <div className="map-wrapper">
+    {/* <div className="map-wrapper">
     <iframe className='googlemap' width="100%" height="600" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=2199%20Smith%20Road%20West%20Chicago,%20IL%2060185%20United%20States+(Nest)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe>
-    </div>
+    </div> */}
 
 
 </div>
